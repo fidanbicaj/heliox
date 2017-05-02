3f352f97e569a636538b0517c6b111bddcb83f25
rexhepberlajolli <rexhepberlajolli@gmail.com>
2017-05-02 20:33:34 +0200

    Updated Login / Register Into one method for both of them

--- heliox/urls.py
+++ heliox/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/', views.login, name='login'),
-    url(r'^register/', views.register, name='register'),
     url(r'^logout/', views.logout, name='logout'),
     url(r'^tasks/', views.tasks, name='tasks'),
 ]
--- heliox/views.py
+++ heliox/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseForbidden
 from .models import Users
 from passlib.hash import sha256_crypt
 from django.contrib import messages
@@ -22,48 +22,46 @@ def logged__in(view_func):
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get("username", "")
-        password = request.POST.get("password", "")
-        if Users.objects.filter(username=username).exists():
-            user = Users.objects.get(username=username)
-            if sha256_crypt.verify(password, user.password):
-                request.session['logged_in'] = True
-                request.session['username'] = username
-                return HttpResponseRedirect('/')
-            else:
-                messages.add_message(request, messages.ERROR, 'Your password is incorrect')
-        else:
-            messages.add_message(request, messages.ERROR, 'Username does not exists')
-        return HttpResponseRedirect('/login')
-    return render(request, 'login.html', {})
-
-
-
-def register(request):
-    if request.method == 'POST':
-        username = request.POST.get("username", "")
-        email = request.POST.get("email", "")
-        password = request.POST.get("password", "")
-        confirm_password = request.POST.get("confirm_password", "")
-        if password == confirm_password:
-            if not Users.objects.filter(username=username).exists():
-                if not Users.objects.filter(email=email).exists():
-                    try:
-                        validate_email(email)
-                    except ValidationError:
-                        messages.add_message(request, messages.ERROR, "Invalid Email Address")
-                        return HttpResponseRedirect('/register')
-                    hashed_password = sha256_crypt.encrypt(password)
-                    Users(username=username, email=email, password=hashed_password).save()
-                    messages.add_message(request, messages.SUCCESS, 'User %s successfully registred' % username)
-                    return HttpResponseRedirect("/login")
+        request_type = request.POST.get("request_type", "")
+        if request_type == 'login':
+            username = request.POST.get("username", "")
+            password = request.POST.get("password", "")
+            if Users.objects.filter(username=username).exists():
+                user = Users.objects.get(username=username)
+                if sha256_crypt.verify(password, user.password):
+                    request.session['logged_in'] = True
+                    request.session['username'] = username
+                    return HttpResponseRedirect('/')
                 else:
-                    messages.add_message(request, messages.ERROR, "Email already exists")
+                    messages.add_message(request, messages.ERROR, 'Your password is incorrect')
             else:
-                messages.add_message(request, messages.ERROR, 'Username already taken')
+                messages.add_message(request, messages.ERROR, 'Username does not exists')
+        elif request_type == 'register':
+            username = request.POST.get("username", "")
+            email = request.POST.get("email", "")
+            password = request.POST.get("password", "")
+            confirm_password = request.POST.get("confirm_password", "")
+            if password == confirm_password:
+                if not Users.objects.filter(username=username).exists():
+                    if not Users.objects.filter(email=email).exists():
+                        try:
+                            validate_email(email)
+                        except ValidationError:
+                            messages.add_message(request, messages.ERROR, "Invalid Email Address")
+                            return HttpResponseRedirect('/login')
+                        hashed_password = sha256_crypt.encrypt(password)
+                        Users(username=username, email=email, password=hashed_password).save()
+                        messages.add_message(request, messages.SUCCESS, 'User %s successfully registered' % username)
+                        return HttpResponseRedirect("/login")
+                    else:
+                        messages.add_message(request, messages.ERROR, "Email already exists")
+                else:
+                    messages.add_message(request, messages.ERROR, 'Username already taken')
+            else:
+                messages.add_message(request, messages.ERROR, 'Passwords do not match')
         else:
-            messages.add_message(request, messages.ERROR, 'Passwords do not match')
-    return render(request, 'register.html', {})
+            return HttpResponseForbidden()
+    return render(request, 'login.html', {})
 
 
 
