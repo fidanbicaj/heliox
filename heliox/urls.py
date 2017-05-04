75797dc56b664ac63226aaaa6c4c951f1c9ea297
kolospukaj <pukajkolos@gmail.com>
2017-05-04 16:24:28 +0200

    Add urls

--- heliox/urls.py
+++ heliox/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^login/', views.login, name='login'),
     url(r'^logout/', views.logout, name='logout'),
     url(r'^tasks/', views.tasks, name='tasks'),
+    url(r'^devices/add/', views.add_device, name='add_devices'),
+    url(r'^devices/remove/', views.remove_device, name='remove_device')
 ]
